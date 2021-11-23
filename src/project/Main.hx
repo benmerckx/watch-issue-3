951aa5e332f0a2444a97917a6c070d480cf2f2e8
Cédric Belin <cedric@belin.io>
2021-11-23 17:39:36 +0100

    Replaced the PHP container by a Node.js one

--- src/project/Main.hx
+++ src/project/Main.hx
@@ -1,14 +1,14 @@
 package project;
 
 import tink.http.Response.OutgoingResponse;
-import tink.http.containers.PhpContainer;
+import tink.http.containers.*;
 import tink.web.routing.Context;
 import tink.web.routing.Router;
 
 class Main {
 	static function main() {
 		final router = new Router<Root>(new Root());
-		PhpContainer.inst.run(request -> router.route(Context.ofRequest(request)).recover(error -> {
+		new NodeContainer(8080).run(request -> router.route(Context.ofRequest(request)).recover(error -> {
 			if (error.code == InternalError) trace(error);
 			OutgoingResponse.reportError(error);
 		}));
